3732733029fe3279184345bf52d03a4b684dfb7e
greenlime21 <greenlime21@gmail.com>
2020-09-22 00:02:59 +0900

    ブログ一覧とfirebaseの連携

--- src/app/user/Blogs.vue
+++ src/app/user/Blogs.vue
@@ -1,35 +1,47 @@
 <template>
 	<div class="blogs">
 		<h1>ブログ一覧ページ</h1>
-		<div class="columns is-variable">
-			<Blog />
-			<Blog />
-			<Blog />
-		</div>
-		<div class="columns is-variable">
-			<Blog />
-			<Blog />
-			<Blog />
-		</div>
-		<div class="columns is-variable">
-			<Blog />
-			<Blog />
-			<Blog />
+		<div class="columns is-multiline">
+			<ul v-for="(blog, index) in blogs" :key="index" class="column is-3 blog-card">
+				<Blog :blog="blog" />
+			</ul>
 		</div>
 	</div>
 </template>
 
 <script>
+import { db } from "~/plugins/firebase.js";
 import Blog from "~/components/Blog";
 
 export default {
 	components: {
 		Blog,
 	},
+	data() {
+		return {
+			blogs: []
+		}
+	},
+	mounted() {
+		db.collection('restaurants/SeRsv7mMLjPmjDATVZqH/blogs').get().then((ref) => {
+			ref.forEach((doc) => {
+				if (!doc.exists) {
+					console.log('No such document!');
+				} else {
+					this.blogs.push(doc.data())
+				}
+			})
+		}).catch(err => {
+			console.log('Error getting document', err);
+		});
+	}
 }
 </script>
 
 <style lang="css" scoped>
+.blog-card {
+	margin: 30px;
+}
 .blogs h1 {
 	margin-top: 40px; 
 }
--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,30 +1,37 @@
 <template>
-	<div>
-		<div class="column card is-10">
+	<div class="card">
+		<router-link to="">
 			<div class="card-image">
-				<figure class="image is-3by2">
-					<img src="https://bulma.io/images/placeholders/480x320.png" alt="Placeholder image">
+				<figure class="image is-1by1">
+					<img :src="blog.imageUrl" alt="Placeholder image">
 				</figure>
 			</div>
 			<div class="card-content">
 				<div class="media">
-					<div>タイトル</div>
+					<div>{{ blog.title }}</div>
 				</div>
 				<div class="content">
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error qui esse corporis et harum provident architecto nostrum? Quia nam pariatur, 
+					{{ blog.body }} 
 					<br>
-					<time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+					<time  datetime="20:00">
+						{{ blog.createdAt.toDate() }}
+					</time>
 				</div>
 			</div>
-		</div>
+		</router-link>
 	</div>
 </template>
 
 <script>
+import { db } from "~/plugins/firebase.js";
+
 export default {
+	props: [
+		'blog'
+	],
 	data() {
 		return {
-			
+
 		}
 	},
 }
